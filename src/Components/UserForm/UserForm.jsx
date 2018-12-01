879b9efc14ea877c23b90c3196c29b3298c1f404
Martin Ayvazyan <martinayvazyan@workfront.com>
2018-12-01 04:05:56 +0400

    add some features

--- src/Components/UserForm/UserForm.jsx
+++ src/Components/UserForm/UserForm.jsx
@@ -1,13 +1,77 @@
 import React, { Component } from 'react';
+import { FormUserDetails } from '../FormUserDetails/FormUserDetails.jsx';
+import { FormPersonalDetails } from '../FormPersonalDetails/FormPersonalDetails';
+import { Confirm } from '../Confirm/Confirm';
 
-export class UserForm extends Component{
+export class UserForm extends Component {
 
-  render(){
-    return (
-      < div>
-         12
-      </div>
-    );
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    city: '',
+    occupation: '',
+    bio: '',
+  };
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value })
+  };
+
+  render () {
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStape={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return (
+          <h1>Confirm</h1>
+
+        );
+      default:
+        return;
+    }
+
   }
 
 }
